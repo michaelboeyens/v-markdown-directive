92e0d4c0dc9467c04b4c252505323afacd23cf2b
Michaël Boeyens <michael.boeyens@gmail.com>
2023-04-03 21:38:18 +0200

    base plugin setup

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,6 @@ import App from './App.vue'
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+app.mount('#app')
---
+++ src/plugins/v-markdown.ts
@@ -0,0 +1,5 @@
+import type { Plugin } from 'vue'
+
+const install: Plugin = { install: (app, options) => {} }
+
+export default install
