716bd4e18be117abed8d1ec9033ddede96af7525
Michaël Boeyens <michael.boeyens@gmail.com>
2023-04-04 20:00:38 +0200

    another small bugfix

--- src/v-markdown-directive.ts
+++ src/v-markdown-directive.ts
@@ -4,12 +4,15 @@ import type { App } from 'vue'
 
 const install = (
   app: App,
-  config: { markedOptions: marked.MarkedOptions; domPurifyOptions: DOMPurify.Config }
+  config: { markedOptions: marked.MarkedOptions; domPurifyOptions: DOMPurify.Config } = {
+    markedOptions: {},
+    domPurifyOptions: {}
+  }
 ) => {
   app.directive('markdown', (el, binding) => {
     const rendered_html = DOMPurify.sanitize(
-      marked(binding.value, config.markedOptions ?? {}),
-      config.domPurifyOptions ?? {}
+      marked(binding.value, config.markedOptions),
+      config.domPurifyOptions
     )
     rendered_html && (el.innerHTML = rendered_html)
   })
