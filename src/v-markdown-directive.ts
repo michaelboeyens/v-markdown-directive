32c7653fe5ecc7292ca9e9870e4a141e2b768f7e
Michaël Boeyens <michael.boeyens@gmail.com>
2023-04-04 19:55:22 +0200

    bugfixes vmarkdown plugin

--- src/v-markdown-directive.ts
+++ src/v-markdown-directive.ts
@@ -1,18 +1,18 @@
-import type { Plugin } from 'vue'
 import { marked } from 'marked'
 import DOMPurify from 'dompurify'
+import type { App } from 'vue'
 
-const install: Plugin<{ markedOptions: marked.MarkedOptions; domPurifyOptions: DOMPurify.Config }> =
-  {
-    install: (app, { markedOptions = {}, domPurifyOptions = {} }) => {
-      app.directive('markdown', (el, binding) => {
-        const rendered_html = DOMPurify.sanitize(
-          marked(binding.value, markedOptions),
-          domPurifyOptions
-        )
-        rendered_html && (el.innerHTML = rendered_html)
-      })
-    }
-  }
+const install = (
+  app: App,
+  config: { markedOptions: marked.MarkedOptions; domPurifyOptions: DOMPurify.Config }
+) => {
+  app.directive('markdown', (el, binding) => {
+    const rendered_html = DOMPurify.sanitize(
+      marked(binding.value, config.markedOptions),
+      config.domPurifyOptions
+    )
+    rendered_html && (el.innerHTML = rendered_html)
+  })
+}
 
 export default install
