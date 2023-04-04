aa4c1e285d0f18aff9377997d13438cf8436c44d
Michaël Boeyens <michael.boeyens@gmail.com>
2023-04-04 20:08:04 +0200

    typescript fixes

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import type VMarkdown from './v-markdown-directive'
-export * from './v-markdown-directive'
-
-declare module 'vue' {
-  interface GlobalDirectives {
-    markdown: typeof VMarkdown
-  }
-}
