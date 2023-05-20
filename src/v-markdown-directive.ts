1796e1efedd81b5d28125ae7a315e1c92eb84d11
Michaël Boeyens <michael.boeyens@gmail.com>
2023-05-20 14:35:49 +0200

    bugfix

--- src/v-markdown-directive.ts
+++ src/v-markdown-directive.ts
@@ -10,7 +10,7 @@ const install = (
   }
 ) => {
   app.directive('markdown', {
-    beforeMount(el, binding) {
+    mounted(el, binding) {
       marked.use(config.markedOptions)
       const rendered_html = DOMPurify.sanitize(marked.parse(binding.value))
       console.log(rendered_html)
