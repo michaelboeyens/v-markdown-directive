89cbb793ad9228ea625ea4b709e639c52a7b3b3e
Michaël Boeyens <michael.boeyens@gmail.com>
2023-07-21 15:52:21 +0200

    cleanup

--- src/v-markdown-directive.ts
+++ src/v-markdown-directive.ts
@@ -13,7 +13,6 @@ const install = (
     mounted(el, binding) {
       marked.use(config.markedOptions)
       const rendered_html = DOMPurify.sanitize(marked.parse(binding.value), config.domPurifyOptions)
-      console.log(rendered_html)
       rendered_html && (el.innerHTML = rendered_html)
     }
   })
