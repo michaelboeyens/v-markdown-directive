f4f7ed6661987ec41e284ef0335c1200588c0804
Michaël Boeyens <michael.boeyens@gmail.com>
2023-07-21 15:52:00 +0200

    bugfix settings dompurify

--- src/v-markdown-directive.ts
+++ src/v-markdown-directive.ts
@@ -12,7 +12,7 @@ const install = (
   app.directive('markdown', {
     mounted(el, binding) {
       marked.use(config.markedOptions)
-      const rendered_html = DOMPurify.sanitize(marked.parse(binding.value))
+      const rendered_html = DOMPurify.sanitize(marked.parse(binding.value), config.domPurifyOptions)
       console.log(rendered_html)
       rendered_html && (el.innerHTML = rendered_html)
     }
