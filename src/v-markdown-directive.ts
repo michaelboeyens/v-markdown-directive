2e866bdd55e362b2a7a6ad079bb5738d3ea00fad
Michaël Boeyens <michael.boeyens@gmail.com>
2023-08-02 21:34:48 +0200

    added doc to install function

--- src/v-markdown-directive.ts
+++ src/v-markdown-directive.ts
@@ -2,6 +2,13 @@ import { marked } from 'marked'
 import type { App } from 'vue'
 import DOMPurify from 'dompurify'
 
+/**
+ * Installs the `markdown` directive to the provided Vue.js `app` instance.
+ * @param app - The Vue.js `app` instance to install the directive to.
+ * @param config - The configuration options for the `markdown` directive.
+ * @param config.markedOptions - The options to pass to the `marked` library.
+ * @param config.domPurifyOptions - The options to pass to the `DOMPurify` library.
+ */
 const install = (
   app: App,
   config: { markedOptions: marked.MarkedOptions; domPurifyOptions: DOMPurify.Config } = {
