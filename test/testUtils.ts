cb148fea85b674d4a53a4a163b1f28b4b39c61d9
Michaël Boeyens <michael.boeyens@gmail.com>
2023-10-05 19:27:31 +0200

    added documentation to self defined testUtils

--- test/testUtils.ts
+++ test/testUtils.ts
@@ -2,6 +2,11 @@ import { mount } from "@vue/test-utils";
 import { expect } from "vitest";
 import markdownPlugin from "../src/v-markdown-directive";
 
+/**
+ * Basic component for testing markdown
+ * @param template html template with v-markdown directive
+ * @param markdown markdown source
+ */
 export const markdownComponent = (template: string, markdown: string) => ({
   template,
   data() {
@@ -11,6 +16,12 @@ export const markdownComponent = (template: string, markdown: string) => ({
   }
 });
 
+/**
+ * Basic test for markdown rendering
+ * @param template html template with v-markdown directive
+ * @param markdown markdown source
+ * @param expected expected html
+ */
 export const basicTest = (template: string, markdown: string, expected: string) => {
   console.log("test: render markdown");
   const wrapper = mount(markdownComponent(template, markdown), {
